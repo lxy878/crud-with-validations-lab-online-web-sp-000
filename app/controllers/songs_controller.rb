bd81f8b9e21b637c59f7cf09df7586794d38f74f
Xiaoyi Liu <lxy878@gmail.com>
2020-06-24 08:45:03 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,5 @@
 class SongsController < ApplicationController
+  before_action: :set_song, only: [:show, :edit, :destroy]
   def index
   end
 
@@ -14,10 +15,17 @@ class SongsController < ApplicationController
   def edit
 
   end
+
   def update
 
   end
+
   def destroy
 
   end
+
+  private
+    def set_song
+      @song = Song.find(params[:id])
+    end
 end
