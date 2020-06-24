4115569d4cc07e5c11159405b45c91093aeb767b
Xiaoyi Liu <lxy878@gmail.com>
2020-06-24 08:36:23 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,6 @@
 class Song < ApplicationRecord
   validates :title, presence: true, uniqueness: {scope: [:artist_name, :release_year], message: "repeated by the same artist in the same year"}
   validates :released, inclusion: { in: [true false] }
-  validates :release_year, inclusion: { in: %w{true false},  }
+  validates :release_year, presence: {if: :released, }
   validates :artist_name, presence: true
-
-  def repeated?
-
-  end
 end
