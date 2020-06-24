20a322a3ca57b5a3f554f2ac405636b47ce31c26
Xiaoyi Liu <lxy878@gmail.com>
2020-06-24 08:54:37 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,7 +11,13 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new()
+    song = Song.new(post_params)
+    if song.valid?
+      song.save
+      redirect_to song_path(song)
+    else
+      render :new
+    end
   end
 
   def edit
